e68b7426318374a28ffd51b317162d6bbde7d477
Irshman <76690755+Irshman@users.noreply.github.com>
2022-02-01 14:44:15 +0200

    fixed

--- js/spript.js
+++ js/spript.js
@@ -5,3 +5,24 @@ btn.addEventListener('click', function() {
     btn.classList.toggle('header__btn-menu--active');
     nav.classList.toggle('header__nav--active');
 });
+
+//========================================================================================================================================================
+// scroll down
+
+const scrollBtn = document.querySelector(".hero__scroll");
+const where = document.querySelector(".capabilities__title");
+
+scrollBtn.addEventListener('click', () => {
+    where.scrollIntoView({ 
+        block: 'center', // к ближайшей границе экрана
+        behavior: 'smooth', // и плавно 
+    });
+})
+
+// $button.addEventListener('click', e => {
+//     // Прокрутим страницу к форме 
+//     $form.scrollIntoView({ 
+//       block: 'nearest', // к ближайшей границе экрана
+//       behavior: 'smooth', // и плавно 
+//     });
+//   });
